645461e40ff0d93e9a85fdef7ce341336ac70490
Trung Dang <dangquoctrung123@gmail.com>
2024-02-26 09:31:41 +0700

    refactor(new_release_check.sh): check for latest version instead of tag
    This fixes a case where we doesn't build older major semver version while it's still maintained

--- .github/scripts/new_release_check.sh
+++ .github/scripts/new_release_check.sh
@@ -5,28 +5,25 @@ set -eu
 REPO_OWNER="pnpm"
 REPO_NAME="pnpm"
 
-# GET ALL OF THE REPO TAGS
-TAGS=$(wget -qO- "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/tags?per_page=1")
+# GET THE LATEST RELEASE OF THE REPO
+RELEASE=$(wget -qO- "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest")
 
-# WE CARE ONLY FOR THE LATEST TAG SO GET ITS URL
-TAG_COMMIT_URL=$(echo $TAGS | jq -r '.[0].commit.url')
 # GET THE TAG NAME AS WE USE IT FOR OUR OWN TAGS
-VERSION=$(echo $TAGS | jq -r '.[0].name')
-# NOW GET THE COMMIT DETAILS
-COMMIT=$(wget -qO- "${TAG_COMMIT_URL}")
-# EXTRACT THE DATE OF THE COMMIT AND MAKE IT A TIMESTAMP
-TIMESTAMP=$(echo $COMMIT | jq -r '.commit.author.date' | date -f - +%s)
+VERSION=$(echo $RELEASE | jq -r '.name')
+
+# EXTRACT THE DATE OF THE RELEASE AND MAKE IT A TIMESTAMP
+TIMESTAMP=$(echo $RELEASE | jq -r '.published_at' | date -f - +%s)
 # GET THE CURRENT TIME
 CURRENT_TIMESTAMP=$(date +%s)
 # AND SUBTRACT 24 HOURS AS WE CHECK 1 TIME PER DAY
 TIMESTAMP_24_HOURS_AGO=$((CURRENT_TIMESTAMP - 86400))
 
-# CHECK IF OUR TAG WAS CREATED WITHIN THE LAST 24 HOURS
-if [[ $TIMESTAMP -gt $TIMESTAMP_24_HOURS_AGO ]]; then
-  echo "Found a new version: $VERSION released within the last 24 hours. Tagging this repository to trigger rebuild"
+# CHECK IF WE ALREADY BUILD THE TAG
+if [ $(git tag -l "$VERSION") ]; then
+  echo "Tag version exists in our repo, nothing to do"
+else
+  echo "Found a new version: $VERSION. Tagging this repository to trigger rebuild"
   TAG_NAME=pnpm$(echo $VERSION | cut -dv -f2)
   git tag $TAG_NAME
   git push origin $TAG_NAME
-else
-  echo "No new releases found, nothing to do"
 fi
