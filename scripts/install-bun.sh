e4eeba2488ad3e1a6c6710d905472601c61bea63
Trung Dang <dangquoctrung123@gmail.com>
2024-09-19 15:54:23 +0700

    feat(install-bun): add inline script to remove compat packages

--- scripts/install-bun.sh
+++ scripts/install-bun.sh
@@ -4,6 +4,18 @@ set -e
 
 # Note: Currently latest (2.35) won't work with bun.
 
+# List of packages to remove  
+packages_to_remove="gcompat glibc glibc-bin"  
+
+for package in $packages_to_remove; do  
+    if apk info -e "$package"; then  
+        echo "Removing $package..."  
+        apk del "$package"  
+    else  
+        echo "$package is not installed, skipping."  
+    fi  
+done  
+
 cd /tmp
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-2.34-r0.apk
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-bin-2.34-r0.apk
