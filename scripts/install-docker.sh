3ef7ff5b5a921d574e5bb34e12d41996471faaf9
Trung Dang <dangquoctrung123@gmail.com>
2025-03-10 01:17:03 +0700

    refactor(install-docker.sh): simplify the script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -4,34 +4,30 @@ set -e
 
 ## What this?
 ## Script to setup docker in a fresh alpine instance :D
-## Credit goes to richart24se's gist: https://gist.github.com/richard24se/336cb2502400a63f4670c751eaca1929, is modified to add login script for zsh, fix usermod not found, remove build packages and fix docker-compose install (now install from registry, install from pip will error).
 
-# change to root and install packages
-su -c "apk add sudo shadow curl openrc"
-# if your user doesn't exists then remove sudo passwords
-USERNAME=$(whoami)
-su -c "grep -qxF '${USERNAME} ALL=(ALL) NOPASSWD: ALL' /etc/sudoers || echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' |  tee -a /etc/sudoers"
-# install docker, docker-compose
-sudo apk add docker docker-compose
+# Install deps
+apk add openrc docker docker-compose
+
 # mount cgroup
-echo "cgroup /sys/fs/cgroup cgroup defaults 0 0" | sudo tee -a  /etc/fstab
-# add perm docker
-sudo usermod -aG docker ${USERNAME}
+grep -qxF 'cgroup /sys/fs/cgroup cgroup defaults 0 0' /etc/fstab || echo 'cgroup /sys/fs/cgroup cgroup defaults 0 0' |  tee -a /etc/fstab
+
 # get start script docker
-curl https://gist.githubusercontent.com/NamesMT/0708cc223a3db878df0477159115a49b/raw/17f45c7f23d3ecd8125963d5171f046979cb8f28/alpine.docker.service.sh --output ${HOME}/alpine.docker.service.sh
-# configure start docker when logging
+wget https://gist.githubusercontent.com/NamesMT/0708cc223a3db878df0477159115a49b/raw/17f45c7f23d3ecd8125963d5171f046979cb8f28/alpine.docker.service.sh -O ${HOME}/alpine.docker.service.sh
+
+# configure start docker on standard shell login
 echo "source $HOME/alpine.docker.service.sh" >> $HOME/.profile
+
 # configure start docker for .zsh if .zshrc exists
 if [ -e $HOME/.zshrc ]
 then
     echo "source $HOME/alpine.docker.service.sh" >> $HOME/.zshrc
 fi
-# start docker
+
+# start docker and verify it's status
 source $HOME/alpine.docker.service.sh
-# verify docker note: first time with sudo
-sudo docker info
-# verify docker-compose
+docker info
 docker-compose version
+
 # cgroup systemd
 cat << EOT > $HOME/alpine.cgroup.systemd.sh
 if [ ! -d /sys/fs/cgroup/systemd ]; then
@@ -43,6 +39,7 @@ if ! grep -qs '/sys/fs/cgroup/systemd' /proc/mounts; then
   echo "mounting cgroup systemd folder..."
 fi
 EOT
+
 # configure start cgroup systemd
 echo "source $HOME/alpine.cgroup.systemd.sh" >> $HOME/.profile
 # configure start cgroup systemd for .zsh if .zshrc exists
